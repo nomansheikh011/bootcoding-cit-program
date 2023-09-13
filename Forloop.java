b7245936034926617cb2dbf907de7362c4cdc6aa
nomansheikh011 <144773396+nomansheikh011@users.noreply.github.com>
2023-09-13 13:23:23 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,13 @@
+public class Forloop
+{
+public static void main(String args[])
+{
+scanner sc = new Scanner(System.in)
+System.out.println("Enter a number");
+int a=sc.nextInt();
+for(int i=1; i<=n;i++)
+{
+System.out.println(i);
+}
+}
+}
